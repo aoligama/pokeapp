aeda0ac4286d6824015c273c54ea7c1118b07a7b
Amanda Gama <amgama17@gmail.com>
2021-01-05 18:38:11 -0300

    Adicionado tooltip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { ListGenerationsComponent } from './pages/list-generations/list-generations.component';
 
 @NgModule({
@@ -30,7 +31,8 @@ import { ListGenerationsComponent } from './pages/list-generations/list-generati
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
